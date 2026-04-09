20f052378a197aba8708122c113383081ba0f2b1
aquibajani <aquib.ajani@obsidi.com>
2026-04-09 09:39:26 -0400

    Code till User Signup

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -8,6 +8,9 @@ import java.util.List;
 import java.util.Optional;
 import java.sql.Timestamp;
 import java.time.Instant;
+import com.bptn.feedApp.exception.domain.EmailExistException;
+import com.bptn.feedApp.exception.domain.UsernameExistException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
@@ -17,6 +20,9 @@ public class UserService {
 	
 	@Autowired
 	EmailService emailService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
@@ -30,13 +36,33 @@ public class UserService {
 		this.userRepository.save(user);
 	}
 	
-	public User signup(User user){
-		user.setUsername(user.getUsername().toLowerCase());
-		user.setEmailId(user.getEmailId().toLowerCase());
-		user.setEmailVerified(false);
-		user.setCreatedOn(Timestamp.from(Instant.now()));
-		this.userRepository.save(user);
-		this.emailService.sendVerificationEmail(user);
-		return user;
-	}
+	public User signup(User user) {
+
+		   user.setUsername(user.getUsername().toLowerCase());
+		   user.setEmailId(user.getEmailId().toLowerCase());
+
+		   this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+		   
+		   user.setEmailVerified(false);
+		   user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+		   user.setCreatedOn(Timestamp.from(Instant.now()));
+
+		   this.userRepository.save(user);
+
+		   this.emailService.sendVerificationEmail(user);
+		   
+		   return user;
+		}
+	
+	private void validateUsernameAndEmail(String username, String emailId) {
+
+        this.userRepository.findByUsername(username).ifPresent(u -> {
+            throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+        });
+
+        this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+            throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+        });
+
+}
 }
